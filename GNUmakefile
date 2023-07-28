e2206c12e4c0dae1dc87185e568d5870b3e166e7
Jiacheng Xu <xjcmaxwellcjx@gmail.com>
2023-07-28 22:31:10 +0800

    Remove unused files

--- GNUmakefile
+++ GNUmakefile
@@ -4,8 +4,3 @@ default: testacc
 .PHONY: testacc
 testacc:
 	TF_ACC=1 go test ./... -v $(TESTARGS) -timeout 120m
-
-.PHONY: generate-terraformtypes
-generate-terraformtypes:
-	go run ./genterraformtypes/main.go
-	go fmt ./akp/types/...
--- akp/apis/v1alpha1/cluster.go
+++ akp/apis/v1alpha1/cluster.go
@@ -10,10 +10,6 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
-//+kubebuilder:object:root=true
-//+kubebuilder:subresource:status
-
-// Cluster is the Schema for the cluster API
 type Cluster struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
@@ -22,19 +18,6 @@ type Cluster struct {
 }
 
 type ClusterSize string
-
-//+kubebuilder:object:root=true
-
-// ClusterList contains a list of Cluster
-type ClusterList struct {
-	metav1.TypeMeta `json:",inline"`
-	metav1.ListMeta `json:"metadata,omitempty"`
-	Items           []Cluster `json:"items"`
-}
-
-func init() {
-	SchemeBuilder.Register(&Cluster{}, &ClusterList{})
-}
 
 type ClusterSpec struct {
 	Description     string      `json:"description,omitempty"`
@@ -48,5 +31,5 @@ type ClusterData struct {
 	Kustomization       runtime.RawExtension `json:"kustomization,omitempty"`
 	AppReplication      *bool                `json:"appReplication,omitempty"`
 	TargetVersion       string               `json:"targetVersion,omitempty"`
-	RedisTunneling      bool                 `json:"redisTunneling,omitempty"`
+	RedisTunneling      *bool                `json:"redisTunneling,omitempty"`
 }
