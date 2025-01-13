0e52a1a1ce911d8a4db30991603d629eabb2781a
xiaopeng <hanxiaop8@outlook.com>
2025-01-13 13:28:46 +0800

    agents impl

--- akp/data_source_akp_kargoagents.go
+++ akp/data_source_akp_kargoagents.go
@@ -1,1 +1,84 @@
 package akp
+
+import (
+	"context"
+	"fmt"
+
+	kargov1 "github.com/akuity/api-client-go/pkg/api/gen/kargo/v1"
+	httpctx "github.com/akuity/grpc-gateway-client/pkg/http/context"
+	"github.com/akuity/terraform-provider-akp/akp/types"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+)
+
+// Ensure provider defined types fully satisfy framework interfaces
+var _ datasource.DataSource = &AkpKargoAgentsDataSource{}
+
+func NewAkpKargoAgentsDataSource() datasource.DataSource {
+	return &AkpKargoAgentsDataSource{}
+}
+
+// AkpKargoAgentsDataSource defines the data source implementation.
+type AkpKargoAgentsDataSource struct {
+	akpCli *AkpCli
+}
+
+func (d *AkpKargoAgentsDataSource) Metadata(ctx context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_kargo_agents"
+}
+
+func (d *AkpKargoAgentsDataSource) Configure(ctx context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+	// Prevent panic if the provider has not been configured.
+	if req.ProviderData == nil {
+		return
+	}
+	akpCli, ok := req.ProviderData.(*AkpCli)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Resource Configure Type",
+			fmt.Sprintf("Expected *AkpCli, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+
+		return
+	}
+	d.akpCli = akpCli
+}
+
+func (d *AkpKargoAgentsDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	tflog.Debug(ctx, "Reading a Kargo Agents Datasource")
+	var data types.KargoAgents
+
+	// Read Terraform configuration data into the model
+	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+	ctx = httpctx.SetAuthorizationHeader(ctx, d.akpCli.Cred.Scheme(), d.akpCli.Cred.Credential())
+
+	apiReq := &kargov1.ListKargoInstanceAgentsRequest{
+		OrganizationId: d.akpCli.OrgId,
+		InstanceId:     data.InstanceID.ValueString(),
+		WorkspaceId:    data.WorkspaceID.ValueString(),
+	}
+	tflog.Debug(ctx, fmt.Sprintf("List Kargo agents request: %s", apiReq))
+	apiResp, err := d.akpCli.KargoCli.ListKargoInstanceAgents(ctx, apiReq)
+	if err != nil {
+		resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to read instance Kargo agents, got error: %s", err))
+		return
+	}
+	tflog.Debug(ctx, fmt.Sprintf("List Kargo agents response: %s", apiResp))
+
+	data.ID = data.InstanceID
+	agents := apiResp.GetAgents()
+	for _, agent := range agents {
+		stateAgent := types.KargoAgent{
+			InstanceID: data.InstanceID,
+			WorkspaceID: data.WorkspaceID,
+		}
+		stateAgent.Update(ctx, &resp.Diagnostics, agent, nil)
+		data.Agents = append(data.Agents, stateAgent)
+	}
+	// Save data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
--- akp/data_source_akp_kargoagents_schema.go
+++ akp/data_source_akp_kargoagents_schema.go
@@ -1,1 +1,35 @@
 package akp
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+)
+
+func (d *AkpKargoAgentsDataSource) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		MarkdownDescription: "Gets information about all Kargo agents attached to an Argo CD instance",
+		Attributes:          getAKPKargoAgentsDataSourceAttributes(),
+	}
+}
+
+func getAKPKargoAgentsDataSourceAttributes() map[string]schema.Attribute {
+	return map[string]schema.Attribute{
+		"workspace_id": schema.StringAttribute{
+			MarkdownDescription: "Workspace ID",
+			Required:            true,
+		},
+		"instance_id": schema.StringAttribute{
+			MarkdownDescription: "Argo CD instance ID",
+			Computed:            true,
+		},
+		"agents": schema.ListNestedAttribute{
+			MarkdownDescription: "List of Kargo agents",
+			Computed:            true,
+			NestedObject: schema.NestedAttributeObject{
+				Attributes: getAKPKargoAgentDataSourceAttributes(),
+			},
+		},
+	}
+}
