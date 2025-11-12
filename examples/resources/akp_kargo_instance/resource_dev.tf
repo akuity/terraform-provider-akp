97119e7d94432f412627c9b3cdef8170e366e5d4
Eddie Webbinaro <5262154+eddiewebb@users.noreply.github.com>
2025-11-12 13:36:22 -0500

    Provide DEX example for OIDC config
    Updated the version of kargo and modified OIDC configuration to enable dex with GitHub OAuth. Removed unrelated configuration.

---
+++ examples/resources/akp_kargo_instance/resource_dev.tf
@@ -0,0 +1,69 @@
+resource "akp_kargo_instance" "example" {
+  name      = "test"
+  workspace = "kargo-workspace"
+  kargo_cm = {
+    adminAccountEnabled  = "true"
+    adminAccountTokenTtl = "24h"
+  }
+  kargo_secret = {
+    adminAccountPasswordHash = "$2a$10$wThs/VVwx5Tbygkk5Rzbv.V8hR8JYYmRdBiGjue9pd0YcEXl7.Kn."
+  }
+  kargo = {
+    spec = {
+      description = "test-description"
+      version     = "v1.8.0-ak.0"
+      // only set one of fqdn and subdomain
+      fqdn      = ${local.kargo_url}
+      subdomain = ""
+      
+      # OIDC using dex with GitHub Oauth
+      oidc_config = {
+        enabled     = true
+        dex_enabled = true
+        dex_config  = <<-EOF
+        connectors:
+          - type: github
+            id: github
+            name: GitHub
+            config:
+              clientID: ${var.GH_OAUTH_CLIENT_ID_KARGO}
+              clientSecret: $GITHUB_CLIENT_SECRET
+              redirectURI: https://${local.kargo_url}/api/dex/callback
+              #orgs:
+              #- name: akuity
+              #preferredEmailDomain: "akuity.io"
+        EOF
+        # Define value for secret referenced above.
+        dex_config_secret = {
+         GITHUB_CLIENT_SECRET = var.GH_OAUTH_CLIENT_SECRET_KARGO
+        }
+        admin_account = {
+          claims = {
+            groups = {
+              values = ["admin-group@example.com"]
+            }
+            email = {
+              values = ["admin@example.com"]
+            }
+            sub = {
+              values = ["admin-sub@example.com"]
+            }
+          }
+        }
+        viewer_account = {
+          claims = {
+            groups = {
+              values = ["viewer-group@example.com"]
+            }
+            email = {
+              values = ["viewer@example.com"]
+            }
+            sub = {
+              values = ["viewer-sub@example.com"]
+            }
+          }
+        }
+      }
+    }
+  }
+}
